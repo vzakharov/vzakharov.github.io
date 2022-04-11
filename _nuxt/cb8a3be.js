c6810fc1b63d78af5541cc16d5556480af5b4014
Vova <vzakharov@gmail.com>
2022-04-11 11:50:22 +0300

    static update

--- _nuxt/cb8a3be.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{235:function(n,o,w){w(236),n.exports=w(237)}},[[235,9,1,10]]]);
---
+++ _nuxt/static/1649666963/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fnotion","\u002Fcopywriting","\u002F","\u002Fcopywriting\u002Forder"]})
