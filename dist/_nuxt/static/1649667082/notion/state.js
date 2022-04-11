98df89cd6ed71113209d408e195cd8c1dd38fd2f
Vova <vzakharov@gmail.com>
2022-04-11 11:52:07 +0300

    fix notion url (static)

---
+++ dist/_nuxt/static/1649667082/notion/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1649667082",layout:"default",error:a,serverRendered:true,routePath:"\u002Fnotion",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8beb5846"}}}}(null));
