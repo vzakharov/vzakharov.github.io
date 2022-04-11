bbc6487c66fc8992a903c3a8d80694c2c310e2ff
Vova <vzakharov@gmail.com>
2022-04-11 18:09:21 +0300

    static update

--- dist/_nuxt/static/1649689387/notion/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1649689387",layout:"default",error:a,serverRendered:true,routePath:"\u002Fnotion",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8159498d"}}}}(null));
