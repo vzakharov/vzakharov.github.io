064ac9809538131606e1029b7e6b6204591bb9d6
Vova <vzakharov@gmail.com>
2022-04-12 16:33:17 +0300

    static update

--- _nuxt/static/1649759984/invoice-generator/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1649759984",layout:"default",error:a,serverRendered:true,routePath:"\u002Finvoice-generator",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"4052c7c1"}}}}(null));
