0114d88e7eef634cf11e99ed79db5f20d45baa69
Vova <vzakharov@gmail.com>
2022-04-12 19:08:11 +0300

    static

--- _nuxt/static/1649779184/copywriting/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1649779184",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcopywriting",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0df05fe5"}}}}(null));
