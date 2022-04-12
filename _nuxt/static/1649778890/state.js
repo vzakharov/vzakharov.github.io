162d5ef92f5c9a6399a1b87eaa47072e0d9608cd
Vova <vzakharov@gmail.com>
2022-04-12 19:00:38 +0300

    static update

--- _nuxt/static/1649778890/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1649778890",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c413802a"}}}}(null,"\u002F"));
