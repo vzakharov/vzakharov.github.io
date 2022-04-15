a1b2926e2862212953361d36b4a9412d66a52219
Vova <vzakharov@gmail.com>
2022-04-15 14:36:31 +0300

    static

--- dist/_nuxt/static/1650018376/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fnotion","\u002Fadmin\u002Fhello","\u002Fcopywriting\u002Forder","\u002Finvoice-generator","\u002Fadmin","\u002Flink","\u002Fcopywriting"]})
