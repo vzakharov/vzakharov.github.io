8b62cba96099fbe3f45d4a4c477f9a7c5e59cfea
Vova <vzakharov@gmail.com>
2022-04-21 18:31:32 +0300

    static

---
+++ _nuxt/static/1650554955/notion/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1650554955",layout:"default",error:a,serverRendered:true,routePath:"\u002Fnotion",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e9e2c962"}}}}(null));
---
+++ _nuxt/static/1650554955/prototyping/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1650554955",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},serverRendered:false,routePath:a,config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:null},content:{dbHash:"e9e2c962"}}}}("\u002Fprototyping"));
