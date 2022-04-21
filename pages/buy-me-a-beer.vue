de6768a41761663daccc4f6c9350b8a25439a0dc
Vova <vzakharov@gmail.com>
2022-04-21 18:28:28 +0300

    remove currency + delayed api call

--- pages/buy-me-a-beer.vue
+++ pages/buy-me-a-beer.vue
@@ -6,10 +6,16 @@
   >
     <h1 class="text-center">Buy me a 🍺</h1>
     If you want to thank me for my work, this is the perfect place to do it.
-    <!-- Div with amount input and currency select dropdown -->
+    <!-- Div with amount input -->
     <b-form class="d-flex align-items-center justify-content-center mt-3 gap-3">
-      How much?
+      <label
+        for="amount"
+        class="fw-bold"
+      >
+        How much?
+      </label>
       <b-input
+        id="amount"
         v-model="amount"
         type="number"
         step="1"
@@ -17,20 +23,31 @@
         placeholder="Amount"
         style="max-width: 100px"
       />
-      <b-select
-        v-model="currency"
-        :options="{ 1: 'USD', 2: 'EUR' }"
-        class="form-select form-control"
-        style="max-width: 100px"
-      />
+      USD
     </b-form>
     <!-- iframe to pay with src = paymentLink -->
     <div
       class="mt-3 d-flex justify-content-center align-items-center"
       style="height: 400px;"
     >
+      <b-alert 
+        v-if="paymentLink == 'Error'"
+        variant="danger"
+        show
+      >
+        <strong>Oops!</strong>
+        <p>
+          Something went wrong. Please try again later.
+        </p>
+        <b-button
+          variant="outline-primary"
+          @click="getPaymentLink()"
+        >
+          Try again
+        </b-button>
+      </b-alert>
       <iframe
-        v-if="paymentLink"
+        v-else-if="paymentLink"
         :src="paymentLink"
         frameborder="0"
         style="width: 100%; height: 100%"
@@ -52,23 +69,27 @@
 
     data() {
 
-      // default: 5 USD
       return {
-        paymentLink: null
+        paymentLink: null,
+        amountChangedTimeout: null
+      }
+
+    },
+
+    mounted() {
+
+      if ( !this.amount ) {
+        this.amount = 5
       }
 
     },
 
     computed: {
 
-      amountAndCurrency() {
-        return `${this.amount} ${this.currency === 1 ? 'USD' : 'EUR'}`
-      },
-
       amount: {
         get() {
           // take from query or default to 5
-          return this.$route.query.amount || 5
+          return this.$route.query.amount
         },
         set(amount) {
           // update route
@@ -80,57 +101,55 @@
             }
           })
         }
-      },
-
-      currency: {
-        get() {
-          // take from query or default to 1
-          return this.$route.query.currency || 1
-        },
-        set(currency) {
-          // update route
-          this.$router.push({
-            ...this.$route,
-            query: {
-              ...this.$route.query,
-              currency
-            }
-          })
-        }
       }
 
     },
 
     watch: {
 
-      amountAndCurrency: {
+      amount: {
         immediate: true,
-        async handler() {
+        handler() { 
 
           this.paymentLink = null
-          try {
 
-            this.paymentLink = (
-              await axios.get(`https://ideality.app/api/topup-link?currency=${this.currency}&amount=${this.amount}`)
-            ).data.link
-
-          } catch (e) {
-            console.error(e)
-            // Show a 'please try again later' toast
-            this.$bvToast.toast(
-              'Please try again later',
-              {
-                title: 'Error',
-                variant: 'danger',
-                solid: true,
-                autoHideDelay: 5000
-              }
-            )
+          // Cancel the timeout if it's already set
+          if ( this.amountChangedTimeout ) {
+            clearTimeout(this.amountChangedTimeout)
           }
+
+          this.amountChangedTimeout = setTimeout(() => {
+            this.getPaymentLink()
+          }, 500)
 
         }
       }
 
+    },
+
+    methods: {
+
+      async getPaymentLink(retry) {
+
+        this.paymentLink = null
+        try {
+
+          this.paymentLink = (
+            await axios.get(`https://ideality.app/api/topup-link?currency=1&amount=${this.amount}`)
+          ).data.link
+
+        } catch (e) {
+          console.error(e)
+          // If it's not a retry, try again in one second
+          if ( !retry ) {
+            setTimeout(() => this.getPaymentLink(true), 1000)
+          } else {
+            // If it's a retry, show an error
+            this.paymentLink = 'Error'
+          }
+        }
+
+      }
     }
     
 
