50e85b8583ed33148e8c783e828c9649d4d8deda
Vova <vzakharov@gmail.com>
2022-04-22 21:18:11 +0300

    flex centered

--- pages/buy-me-a-beer.vue
+++ pages/buy-me-a-beer.vue
@@ -1,61 +1,70 @@
 <!-- A buy-me-a-beer page -->
 <template>
   <div 
-    style="position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); min-width: 512px;" 
-    class="rounded shadow p-3"
+    class="d-flex align-items-center justify-content-center vw-100 vh-100"
   >
-    <h1 class="text-center">Buy me a 🍺</h1>
-    If you want to thank me for my work, this is the perfect place to do it.
-    <!-- Div with amount input -->
-    <b-form class="d-flex align-items-center justify-content-center mt-3 gap-3">
-      <label
-        for="amount"
-        class="fw-bold"
-      >
-        How much?
-      </label>
-      <b-input
-        id="amount"
-        v-model="amount"
-        type="number"
-        step="1"
-        min="1"
-        placeholder="Amount"
-        style="max-width: 100px"
-      />
-      USD
-    </b-form>
-    <!-- iframe to pay with src = paymentLink -->
-    <div
-      class="mt-3 d-flex justify-content-center align-items-center"
-      style="height: 400px;"
+    <div 
+      class="rounded shadow p-3"
+      style="max-width: 600px; max-height: 600px"
     >
-      <b-alert 
-        v-if="paymentLink == 'Error'"
-        variant="danger"
-        show
-      >
-        <strong>Oops!</strong>
-        <p>
-          Something went wrong. Please try again later.
-        </p>
-        <b-button
-          variant="outline-primary"
-          @click="getPaymentLink()"
+      <h1 class="text-center">Buy me a 🍺</h1>
+      <div class="d-sm-none">
+        You could buy me a beer here, but the payment widget doesn't work on mobile, so please come back on a desktop device. Thanks!
+      </div>
+      <div class="d-none d-sm-block">
+        If you want to thank me for my work, this is the perfect place to do it.
+        <!-- Div with amount input -->
+        <b-form class="d-flex align-items-center justify-content-center mt-3 gap-3">
+          <label
+            for="amount"
+            class="fw-bold"
+          >
+            How much?
+          </label>
+          <b-input
+            id="amount"
+            v-model="amount"
+            type="number"
+            step="1"
+            min="1"
+            placeholder="Amount"
+            style="max-width: 100px"
+          />
+          USD
+        </b-form>
+        <!-- iframe to pay with src = paymentLink -->
+        <div
+          class="mt-3 d-flex justify-content-center align-items-center"
+          style="height: 800px;"
         >
-          Try again
-        </b-button>
-      </b-alert>
-      <iframe
-        v-else-if="paymentLink"
-        :src="paymentLink"
-        frameborder="0"
-        style="width: 100%; height: 100%"
-      />
-      <!-- A please-wait message otherwise -->
-      <div v-else class="text-center">
-        <b-spinner type="grow" variant="primary" />
-        <span class="ml-2">Please wait, connecting to payment provider...</span>
+          <b-alert 
+            v-if="paymentLink == 'Error'"
+            variant="danger"
+            show
+          >
+            <strong>Oops!</strong>
+            <p>
+              Something went wrong. Please try again later.
+            </p>
+            <b-button
+              variant="outline-primary"
+              @click="getPaymentLink()"
+            >
+              Try again
+            </b-button>
+          </b-alert>
+          <iframe
+            v-else-if="paymentLink"
+            :src="paymentLink"
+            frameborder="0"
+            style="width: 100%; height: 100%"
+          />
+          <!-- A please-wait message otherwise -->
+          <div v-else class="text-center">
+            <b-spinner type="grow" variant="primary" />
+            <span class="ml-2">Please wait, connecting to payment provider...</span>
+          </div>
+        </div>
       </div>
     </div>
   </div>
